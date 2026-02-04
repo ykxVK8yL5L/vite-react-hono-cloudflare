760249a665806bd785df8fee6760840bf08779a7
ykxVK8yL5L <58539637+ykxVK8yL5L@users.noreply.github.com>
2026-02-04 13:52:06 +0800

    添加环境变量

--- src/worker/index.ts
+++ src/worker/index.ts
@@ -1,6 +1,10 @@
 import { Hono } from "hono";
-const app = new Hono<{ Bindings: Env }>();
 
+type Bindings = {
+  BEARER_TOKEN: string;
+};
+
+const app = new Hono<{ Bindings: Bindings }>();
 app.get("/api/", (c) => c.json({ name: "Cloudflare" }));
 app.get("/api/user", (c) => c.json({ name: "test user query" }));
 app.post("/api/login", (c) => c.json({ name: "Cloudflare", token: "dummy-token" }));
